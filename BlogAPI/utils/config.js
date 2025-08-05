9992aa317a7425cd445ecac5d519af8743673779
Enmanuel Urbina <enmanuelu100@gmail.com>
2025-08-04 22:57:17 -0600

    cerrando jornada aprendiendo manejo de errores usando middlewares

--- BlogAPI/utils/config.js
+++ BlogAPI/utils/config.js
@@ -1,11 +1,12 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 const PORT = process.env.PORT ?? 3000
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 export {
   PORT,
   MONGODB_URI
 }
-
